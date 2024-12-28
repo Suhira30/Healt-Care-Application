88a0b94955fb554f87b09c4a651482bd133a61b5
Suhira Balarajan <121123475+Suhira30@users.noreply.github.com>
2024-12-28 18:25:27 +0530

    Update FirebaseAuth.js
    api key

--- Services/FirebaseAuth.js
+++ Services/FirebaseAuth.js
@@ -7,7 +7,7 @@ import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA7GsSdSoJGCvNjGeSnQeZrSsupGUPM-4s",
+  apiKey: "",
   authDomain: "auth-service-mad.firebaseapp.com",
   projectId: "auth-service-mad",
   storageBucket: "auth-service-mad.firebasestorage.app",
